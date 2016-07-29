154793c83aa21856a471871b3a813e4bb486eb08
datafarm_assist <takenori@datafarm.jp>
2016-07-29 17:58:34 +0900

    update demo

--- main.js
+++ main.js
@@ -3,12 +3,18 @@ window.shapes = {};
 window.selectRectNumber = null;
 window.fillObjects = [];
 window.shapes = [];
+window.b64String = "";
 
 // canvasに、CreateJSのステージを作成
 stage = new createjs.Stage("demo");
 
 // マウス操作を可能にする
 stage.mouseEnabled = true;
+stage.enableMouseOver();
+
+// カラーボタンを無効に
+$(".ColorButton").prop("disabled", true);
+
 
 // 描写したい矩形のセットを定義
 petternArr01 = [
@@ -17,13 +23,19 @@ petternArr01 = [
 		y: 10,
 		width: 100,
 		height: 100,
-		color: "#EA4335"
+		color: "#d3d3d3"
 	}, {
 		x: 120,
 		y: 10,
 		width: 100,
 		height: 100,
-		color: "pink"
+		color: "#d3d3d3"
+	}, {
+		x: 230,
+		y: 10,
+		width: 100,
+		height: 100,
+		color: "#d3d3d3"
 	}
 ];
 
@@ -31,7 +43,7 @@ petternArr01 = [
 function drawRects(arr) {
 
 	for (var i = 0; i < arr.length; i++) {
-		
+
 		var t = arr[i];
 
 		shapes[i] = new createjs.Shape();
@@ -50,24 +62,59 @@ function drawRects(arr) {
 			var n = i;
 			return function () {
 				selectRectNumber = n; // 選択中の矩形を変更
-				shapes[selectRectNumber].shadow = new createjs.Shadow(createjs.Graphics.getRGB(0,0,0), 8,8,8,8);
+				$(".ColorButton").prop("disabled", false); // カラーボタンを有効に
+				addShadowSelectedRect(selectRectNumber, shapes); // 選択中の矩形に、影を追加
 				stage.update();
 			}
 		})();
 
-		// 矩形にクリックイベントを定義
+		// 矩形にクリックイベントを追加
 		shapes[i].addEventListener("click", f);
 
-		// ステージに各矩形を追加
-		stage.addChild(shapes[i]);
-		// 描写
-		stage.update();
+		// 矩形にマウスオーバー/アウト時のイベントを追加
+		shapes[i].addEventListener("mouseover", function (e) {
+			document.body.style.cursor = "pointer";
+			console.log("test");
+		});
+		shapes[i].addEventListener("mouseout", function (e) {
+			document.body.style.cursor = "";
+		});
+
+		stage.addChild(shapes[i]);// ステージに各矩形を追加
+		stage.update();// 描写
 	}
 }
 
 // 矩形の描写
 drawRects(petternArr01);
 
+// 選択した矩形に、影を描写する関数
+function addShadowSelectedRect(number, shapesArr) {
+
+	// まず、すべての矩形の影を消去する
+	for (var i = 0; i < shapesArr.length; i++) {
+		shapesArr[i].shadow = null; // shape.shadow = null とすると、影を消去できる。
+	}
+
+	shapesArr[number].shadow = new createjs.Shadow("#17b7ff", 0, 0, 20); // 選択中の矩形に影を追加
+	stage.update(); // 描写
+
+}
+
+// 矩形の選択をすべて解除する関数
+function clearSelect(){
+
+	selectRectNumber = null; // 選択中の番号を空に
+	
+	// すべての矩形の影を消去する
+	for (var i = 0; i < shapes.length; i++) {
+		shapes[i].shadow = null; // shape.shadow = null とすると、影を消去できる。
+	}
+	
+	$(".ColorButton").prop("disabled", true); // カラーボタンを無効に
+	
+	stage.update(); // 描写
+}
 
 // 色を変更する関数
 function changeColor(color) {
@@ -75,6 +122,7 @@ function changeColor(color) {
 	if (selectRectNumber != null) {
 		fillObjects[selectRectNumber].style = color; // fillObjectから、色を変更する
 		stage.update(); // ステージの更新
+		getBase64();
 	} else {
 		alert("選択してください");
 	}
@@ -82,6 +130,17 @@ function changeColor(color) {
 
 // canvasをbase64に変換する関数
 function getBase64() {
-	var b64 = $("#demo")[0].toDataURL();
-	$("#base64text").text(b64);
+	b64String = $("#demo")[0].toDataURL();
 }
+
+// 送信
+function send() {
+	
+	clearSelect(); // 選択の解除
+	getBase64(); // base64を取得
+	
+	$("img#usersAnswer").attr("src", b64String); // imgに出力
+	$("pre#codeBox").text(b64String); // codeに出力
+	
+}
+
